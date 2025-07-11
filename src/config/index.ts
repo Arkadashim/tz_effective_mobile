7d59a82da9c5e5bc8927caeacced52a491374635
arkadashiim <arkadashiim@mail.ru>
2025-07-11 14:12:31 +0300

    fix: config

--- src/config/index.ts
+++ src/config/index.ts
@@ -13,11 +13,11 @@ export const config: {
 } = {
   database: {
     type: 'postgres',
-    host: process.env.DATABASE_HOST || 'localhost',
-    port: parseInt(process.env.DATABASE_PORT || '5432', 10),
-    username: process.env.DATABASE_USERNAME || 'postgres',
-    password: process.env.DATABASE_PASSWORD || 'postgres',
-    database: process.env.DATABASE_NAME || 'effective_mobile',
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT || '5432', 10),
+    username: process.env.DB_USERNAME || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres',
+    database: process.env.DB_NAME || 'effective_mobile',
     entities: ['src/entities/*.ts'],
     synchronize: process.env.NODE_ENV !== 'production',
   },
